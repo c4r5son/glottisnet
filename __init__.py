a3b4eabeb576f347ab277118286c47fb533461e9
cstillman <cstillman@email.wm.edu>
2022-04-21 10:54:16 -0400

    added init file

---
+++ __init__.py
@@ -0,0 +1,8 @@
+from glottisnet import glottisnet
+
+import warnings
+warnings.filterwarnings(
+    action="ignore", 
+    message="This overload of nonzero is deprecated:\n\tnonzero()", 
+    category=UserWarning
+)
